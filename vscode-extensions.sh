5532ab1c4ed04c6f56a6693432ceb6a04d4ac4fa
oneunreadmail <github@oneunreadmail.com>
2024-05-17 11:59:27 -0500

    Added vscode-extensions.sh

---
+++ vscode-extensions.sh
@@ -0,0 +1,6 @@
+code -v
+code --install-extension ms-python.python
+code --install-extension eamodio.gitlens
+code --install-extension charliermarsh.ruff
+code --install-extension BazelBuild.vscode-bazel
+code --install-extension redhat.vscode-yaml
