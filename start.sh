868a8e6b1ac05605fc05afb6d8c01006646c98f1
oneunreadmail <github@oneunreadmail.com>
2024-12-09 14:45:01 -0600

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+TARGET='//junk/ohmmeter:ohmmeter_venv'
+VENV_LOCATION='.ohmmeter_venv'
+WORKSPACE_DIR="/workspaces/jupyter"
+HOSTNAME=$(hostname).coder.pods.max.avride.ai
+
+if [ ! -d "$WORKSPACE_DIR" ]; then
+    git clone https://github.com/avride/av "$WORKSPACE_DIR"
+fi
+
+cd "$WORKSPACE_DIR"
+git pull || echo "Could not pull latest changes automatically."
+
+tmux new-session -d -s jupyter || echo "Session already exists."
+sleep 5
+tmux send-keys -t jupyter "cd $WORKSPACE_DIR" C-m
+tmux send-keys -t jupyter "bazelisk run $TARGET" C-m
+tmux send-keys -t jupyter "source $VENV_LOCATION/bin/activate" C-m
+tmux send-keys -t jupyter "cd /workspaces/av" C-m
+tmux send-keys -t jupyter "jupyter notebook --ip '::' --NotebookApp.token='' C-m
