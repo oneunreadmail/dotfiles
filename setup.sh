c486cbc703fb597a15b53cec9f8e43095ae7e192
oneunreadmail <github@oneunreadmail.com>
2024-12-11 16:02:07 -0600

    updated with stuff

--- setup.sh
+++ setup.sh
@@ -4,11 +4,9 @@ set -e
 
 TARGET='//junk/ohmmeter:ohmmeter_venv'
 VENV_LOCATION='.ohmmeter_venv'
-WORKSPACE_DIR="/workspaces/jupyter"
+WORKSPACE_DIR='/workspaces/jupyter'
 HOSTNAME=$(hostname).coder.pods.max.avride.ai
-
-echo $USER
-export
+NOTEBOOK_DIR=/workspaces/av/junk/$GITHUB_USER
 
 if [ ! -d "$WORKSPACE_DIR" ]; then
     git clone https://github.com/avride/av "$WORKSPACE_DIR"
@@ -16,12 +14,7 @@ fi
 
 cd "$WORKSPACE_DIR"
 git pull || echo "Could not pull latest changes automatically."
-
-# su -l vscode -c "
-#     bazelisk run $TARGET
-#     sleep 1
-#     nohup $VENV_LOCATION/bin/jupyter lab --ip '::' --no-browser --IdentityProvider.token='' &
-# "
+mkdir -p $NOTEBOOK_DIR
 
 PATH="/home/vscode/.nix-profile/bin:$PATH"
 tmux new-session -d -s jupyter || echo "Session already exists."
@@ -29,5 +22,12 @@ sleep 5
 tmux send-keys -t jupyter "cd $WORKSPACE_DIR" C-m
 tmux send-keys -t jupyter "bazelisk run $TARGET" C-m
 tmux send-keys -t jupyter "source $VENV_LOCATION/bin/activate" C-m
-tmux send-keys -t jupyter "cd /workspaces/av" C-m
-tmux send-keys -t jupyter "jupyter lab --ip '::' --no-browser --IdentityProvider.token=''" C-m
+tmux send-keys -t jupyter "
+    jupyter lab \
+    --ip '::' \
+    --no-browser \
+    --IdentityProvider.token='' \
+    --ServerApp.root_dir='/workspaces/av' \
+    --ServerApp.notebook_dir=$NOTEBOOK_DIR \
+    --NotebookApp.custom_display_url=http://$HOSTNAME:8888 \
+" C-m
