f9207bf7fd8319866e40a0074ddb23eebf562b31
oneunreadmail <github@oneunreadmail.com>
2025-09-23 14:34:21 -0500

    Add multiple VS Code extensions to installation script

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,6 +1,12 @@
 code -v
-code --install-extension ms-python.python
-code --install-extension eamodio.gitlens
 code --install-extension charliermarsh.ruff
-code --install-extension BazelBuild.vscode-bazel
+code --install-extension eamodio.gitlens
+code --install-extension gaborv.flatbuffers
+code --install-extension github.vscode-pull-request-github
+code --install-extension google.gemini-cli-vscode-ide-companion
+code --install-extension llvm-vs-code-extensions.vscode-clangd
+code --install-extension lucien-martijn.parquet-visualizer
+code --install-extension ms-python.python
 code --install-extension redhat.vscode-yaml
+code --install-extension samuelcolvin.jinjahtml
+code --install-extension StackBuild.bazel-stack-vscode
